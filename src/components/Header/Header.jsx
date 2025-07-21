0aea9ff6835ea494b7519fbee6727a8f190f5e1f
cioccalezombi <86154284+cioccalezombi@users.noreply.github.com>
2025-07-20 22:07:23 -0300

    navbar lista
    navbar lista. falta experimentar con alguna fuente pero el resto ya esta

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,50 @@
 import { Link } from "react-router-dom";
-import { FaShoppingCart } from "react-icons/fa";
-import desktopLogo from "../../assets/banner.png";
-import mobileLogo from "../../assets/banner1.png";
-import { useEffect, useState } from "react";
 import "./Header.css";
+import banner from "../../assets/banner.png";
+import banner1 from "../../assets/banner1.png";
+import { useEffect, useState } from "react";
 
 const Header = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
   useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <header className="header-container">
-      <div className="header-logo">
-        <Link to="/">
-          <img
-            src={isMobile ? mobileLogo : desktopLogo}
-            alt="Black Library Shop"
-            className="logo-img"
-          />
-        </Link>
-      </div>
+    <header className="container-fluid bg-white border-bottom custom-header">
+      <div className="row align-items-center">
+        <div className="col-12 col-md-3 text-center text-md-start mb-2 mb-md-0">
+          <Link to="/">
+            <img
+              src={isMobile ? banner1 : banner}
+              alt="Logo"
+              className="img-fluid custom-logo"
+            />
+          </Link>
+        </div>
 
-      <nav className="header-nav">
-        <Link to="/categoria/herejiaDeHorus">Herejía de Horus</Link>
-        <Link to="/categoria/warhammer40K">Warhammer 40K</Link>
-        <Link to="/categoria/ageOfSigmar">Age of Sigmar</Link>
-        <Link to="/categoria/personajesIconicos">Personajes Icónicos</Link>
-      </nav>
+        <nav className="col-12 col-md-6 d-flex flex-column flex-md-row justify-content-center align-items-center gap-2">
+          {!isMobile && (
+            <Link to="/" className="nav-link-hover">Inicio</Link>
+          )}
+          <Link to="/categoria/herejiaDeHorus" className="nav-link-hover">Herejía de Horus</Link>
+          <Link to="/categoria/warhammer40K" className="nav-link-hover">Warhammer 40K</Link>
+          <Link to="/categoria/ageOfSigmar" className="nav-link-hover">Age of Sigmar</Link>
+          <Link to="/categoria/personajesIconicos" className="nav-link-hover">Personajes Icónicos</Link>
+        </nav>
 
-      <div className="header-cart">
-        <Link to="/ordenes">
-          <FaShoppingCart /> Órdenes
-        </Link>
+        <div className="col-12 col-md-3 d-flex justify-content-end mt-2 mt-md-0">
+          <div className="header-cart-icons d-flex gap-3">
+            <Link to="/cart" className="nav-link-hover">🛒</Link>
+            <Link to="/ordenes" className="nav-link-hover">📄</Link>
+          </div>
+        </div>
       </div>
     </header>
   );
